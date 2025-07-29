56aba7cee2e0235d3c9b9d3fdf2fc2bd9bac32e9
Abolfazl Khezri <abolfazl.khezri.business@gmail.com>
2025-07-29 14:54:09 +0330

    Create Baae of models & views else: CoreApp --> views

--- BackEnd/AuthenticationSystem/models.py
+++ BackEnd/AuthenticationSystem/models.py
@@ -1,3 +1,80 @@
 from django.db import models
+from django.contrib.auth.models import (
+    BaseUserManager,
+    AbstractBaseUser,
+    Group,
+    Permission,
+    PermissionsMixin,
+)
 
-# Create your models here.
+
+class CustomUserManager(BaseUserManager):
+    def create_normal(self, user_name=None, user_type="normal"):
+        if not user_name:
+            raise ValueError("The 'user_name' is required")
+
+        try:
+            user = self.model(
+                user_name=user_name,
+                user_type=user_type,
+            )
+            user.save(using=self._db)
+        except ValueError as e:
+            raise e
+
+    def create_admin(self, user_name=None, user_type="admin"):
+        if not user_name:
+            raise ValueError("The 'user_name' is required")
+
+        try:
+            user = self.model(
+                user_name=user_name,
+                user_type=user_type,
+            )
+        except ValueError as e:
+            raise e
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+
+    USER_TYPES = [
+        ("admin", "Admin"),
+        ("noramal", "Normal"),
+    ]
+
+    user_name = models.CharField(
+        unique=True,
+        blank=False,
+        null=False,
+    )
+    user_type = models.CharField(
+        max_length=50,
+        choices=USER_TYPES,
+        default="normal",
+    )
+    groups = models.ManyToManyField(
+        Group,
+        verbose_name="groups",
+        blank=True,
+        help_text="The groups this user belongs to. A user will get all permissions granted to each of their groups.",
+        related_name="customuser_set",
+        related_query_name="customuser",
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        verbose_name="user permissions",
+        blank=True,
+        help_text="Specific permissions for this user.",
+        related_name="customuser_set",
+        related_query_name="customuser",
+    )
+
+    def get_by_natural_key(self, user_name):
+        return self.get(user_name=user_name)
+
+    objects = CustomUserManager()
+    USERNAME_FIELD = "id"
+    REQUIRED_FIELDS = ["first_name", "last_name"]
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
--- BackEnd/AuthenticationSystem/views.py
+++ BackEnd/AuthenticationSystem/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import CustomUser
 
-# Create your views here.
+
+def check_exist_user(user_name):
+    user = CustomUser.objects.filter(user_name=user_name).exists()
+    if user:
+        return True
+    else:
+        return False
+
+
+@api_view(["POST"])
+def create_user(request):
+    user_user_type = request.data.get("user_type")
+    user_user_name = request.data.get("user_name")
+
+    if not all([user_user_name, user_user_type]):
+        return Response(
+            {"error": "all fields are required"},
+            status=status.HTTP_400_BAD_REQUEST,
+        )
+    if (check_exist_user(user_user_name)) == False:
+        user = CustomUser.create_normal(user_name=user_user_name)
+        user.save()
+
+
+@api_view(["POST"])
+def create_admin(request):
+    user_user_type = request.data.get("user_type")
+    user_user_name = request.data.get("user_name")
+
+    if not all([user_user_name, user_user_type]):
+        return Response(
+            {"error": "all fields are required"},
+            status=status.HTTP_400_BAD_REQUEST,
+        )
+    if (check_exist_user(user_user_name)) == False:
+        user = CustomUser.create_admin(user_name=user_user_name)
+        user.save()
